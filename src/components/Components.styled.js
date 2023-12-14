2ebc5de0ce6b54541af11d50424a4dc0ff9d36af
Maksym-Kartawcew <maxkartavtsev@gmail.com>
2023-12-14 14:37:59 +0100

    Styles starts

--- src/components/Components.styled.js
+++ src/components/Components.styled.js
@@ -4,9 +4,15 @@ import styled from "styled-components";
 export const Container = styled.div`
   margin: 0 auto;
   max-width: 1000px;
+  height: 100vh;
 `;
 
 export const NavigationBox = styled.ul`
   display: flex;
-  justify-content: space-between;
+  gap: 50px;
+  margin: 0 auto;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  justify-content: center;
+  color: white;
 `;
